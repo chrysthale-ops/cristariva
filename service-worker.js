41cdfe508045c892352a9b1f6355d17808dbe9eb
chrysthale-ops <chrysthale@gmail.com>
2026-09-12 23:10:21 +0200

    Fix card 24 Regrets and refresh PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cristariva-modele-a-v5-20260912-card24-final';
+const CACHE_NAME='cristariva-modele-a-v6-20260912-card24-corrigee';
 const SHELL=['./','./index.html','./manifest.webmanifest','./manifest-en.webmanifest','./icon-192.png','./icon-512.png','./cards/024.webp'];
 
 self.addEventListener('install',event=>{
