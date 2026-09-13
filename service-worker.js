f0fa47642157928969f142731e586751f891b6c1
CRISTARIVA updater <actions@users.noreply.github.com>
2026-09-13 18:33:26 +0000

    Synchroniser définitions et interprétations avec les noms courts

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cristariva-modele-a-v11-20260913-titres-courts-images';
+const CACHE_NAME='cristariva-modele-a-v12-20260913-noms-courts-textes';
 const SHELL=['./','./index.html','./manifest.webmanifest','./manifest-en.webmanifest','./icon-192.png','./icon-512.png','./cards/024.webp','./cards/109.webp'];
 
 self.addEventListener('install',event=>{
