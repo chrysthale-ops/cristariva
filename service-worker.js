9b6ccac9d29c0fe47fe8d135b0a2664163859a61
CRISTARIVA updater <actions@users.noreply.github.com>
2026-09-13 16:50:30 +0000

    Synchroniser les noms courts validés des 130 cartes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cristariva-modele-a-v9-20260913-crush-final';
+const CACHE_NAME='cristariva-modele-a-v10-20260913-noms-courts';
 const SHELL=['./','./index.html','./manifest.webmanifest','./manifest-en.webmanifest','./icon-192.png','./icon-512.png','./cards/024.webp','./cards/109.webp'];
 
 self.addEventListener('install',event=>{
